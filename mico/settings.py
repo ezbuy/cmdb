2aebe78905fad4f19694c4ea35d7e94ebceaef59
mico <pengzihe@ezbuy.com>
2016-10-09 16:05:40 +0800

    nginx django.conf

--- mico/settings.py
+++ mico/settings.py
@@ -141,5 +141,6 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = '/django/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,"static"),)
