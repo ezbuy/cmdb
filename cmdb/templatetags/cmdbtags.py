0288e7a0a811e8659530b03ab495cb7178346a46
mico <pengzihe@ezbuy.com>
2016-12-29 13:46:52 +0800

    update cmdb/templatetags/cmdbtags.py

--- cmdb/templatetags/cmdbtags.py
+++ cmdb/templatetags/cmdbtags.py
@@ -8,6 +8,11 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    group = Group.objects.get(name=group_name)
-    return True if group in user.groups.all() else False
+    try:
+        group = Group.objects.get(name=group_name)
+        return True if group in user.groups.all() else False
+
+    except Exception, e:
+        print e
+        return False
 
