5bfc775e2443ab58762ff36d2f1a97e5fff13adf
mico <pengzihe@ezbuy.com>
2017-01-04 18:02:18 +0800

    dashboard update

--- cmdb/views.py
+++ cmdb/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render,render_to_response
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.models import User
+from django.utils.timezone import now, timedelta
+from django.db.models import Count
+from asset.models import gogroup,Asset
+from logs.models import goLog
 # Create your views here.
 @login_required
 def index(request):
-    return render(request,'base.html')
+    users = User.objects.count()
+    projects =  gogroup.objects.count()
+    start = now().date()
+    end = start + timedelta(days=1)
+    build_count = goLog.objects.filter(datetime__range=(start,end)).count()
+    top_three = goLog.objects.filter(datetime__range=(start,end)).values('user').annotate(count=Count('user')).order_by('-count')[0:3]
+    hosts_count = Asset.objects.all().count()
+
+    return render(request,'dashboard.html',{'users':users,'projects':projects,'build_count':build_count,'top_three':top_three,'hosts_count':hosts_count})
