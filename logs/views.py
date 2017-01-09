2618b4a92a51f5c2b6604b30915e99ee8e027004
mico <pengzihe@ezbuy.com>
2017-01-09 22:49:52 +0800

    paginator for logs

--- logs/views.py
+++ logs/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render,render_to_response,HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
-from logs.models import *
+from logs.models import goLog
 
 @login_required
 def logs(request):
-    logs = goLog.objects.all().order_by('-id')[:10]
-    return render(request,'logs/logs.html',{'logs':logs})
+    logs_list = goLog.objects.all()
+    paginator = Paginator(logs_list,10)
+    page = request.GET.get('page')
+
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+
+    return render(request,'logs/logs.html',{'logs':contacts})
