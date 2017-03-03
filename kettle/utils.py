677341c993389e5919d570843f670b9f82e48b0e
mico <pengzihe@ezbuy.com>
2017-03-03 15:48:24 +0800

    kettle

--- kettle/utils.py
+++ kettle/utils.py
@@ -1,16 +1,34 @@
 from celery.task import task
 from salt_api.api import SaltApi
-from mico.settings import kettle_host,kettle_install_dir,kettle_svn_path
+from mico.settings import kettle_host,kettle_install_dir,kettle_svn_path,kettle_log_path
+from asset.utils import notification,logs
+
 
 @task
-def kettle_run(cmd_type,file_path):
+def kettle_run(user,ip,cmd_type,file_path,kettle_log_file):
     salt_api = SaltApi()
     file_path = kettle_svn_path + file_path
-    if int(cmd_type) == 1:
-        cmd = '%span.sh -file %s' % (kettle_install_dir,file_path)
-    elif int(cmd_type) == 2:
-	    cmd = '%skitchen.sh -file %s' % (kettle_install_dir,file_path)
+    kettle_log_file = kettle_log_path + kettle_log_file
 
+    if int(cmd_type) == 1:
+        cmd = '%span.sh -file %s -logfile %s' % (kettle_install_dir,file_path,kettle_log_file)
+    elif int(cmd_type) == 2:
+        cmd = '%skitchen.sh -file %s -logfile %s' % (kettle_install_dir,file_path,kettle_log_file) 
+
+    exists_file_cmd = 'ls %s' % file_path
+    exists_file = {
+        'client': 'local',
+        'tgt': kettle_host,
+        'fun': 'cmd.run',
+        'arg': exists_file_cmd
+    }
+
+    file_result = salt_api.salt_cmd(exists_file)
+    if file_result['return'][0][kettle_host] != file_path:
+        logs(user,ip,cmd,'not %s file.' % file_path)
+        notification(kettle_host,'kettle job','it is error',user)
+        return 0
+   
     data = {
     	'client': 'local',
         'tgt': kettle_host,
@@ -21,6 +39,9 @@ def kettle_run(cmd_type,file_path):
     result = salt_api.salt_cmd(data)
     if result != 0:
     	result = result['return']
- 
+    
+    logs(user,ip,cmd,'running')
+    notification(kettle_host,'kettle job',result,user)
     return result
+
 
--- kettle/views.py
+++ kettle/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render,HttpResponse
 from django.contrib.auth.decorators import login_required
 from utils import kettle_run
+from asset.utils import deny_resubmit
+
+
 
 # Create your views here.
 
 @login_required
+@deny_resubmit(page_key='kettle')
 def kettle_index(request):
 	return render(request,'kettleIndex.html')
 
 
 @login_required
+@deny_resubmit(page_key='kettle')
 def kettle_execute(request):
-    cmd_type = request.GET['type']
-    file_path = request.GET['file']
+    cmd_type = request.POST['type']
+    kettle_file = request.POST['file']
+    kettle_log_file = request.POST['kettle_log_file']
     ip = request.META['REMOTE_ADDR']
     username = request.user
-    return_date = kettle_run.delay(cmd_type,file_path)
-    print return_date
-    result = [{'kettle':'The kettle is running,please check kettle log file!'}]
+    return_date = kettle_run.delay(username,ip,cmd_type,kettle_file,kettle_log_file)
+    #print return_date
+    result = [{'kettle':'The kettle is running,please check kettle log file(%s)!' % kettle_log_file}]
     return render(request,'getdata.html',{'result':result})
 
