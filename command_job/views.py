978cdb521be3c13e6416c744da9585c83ed779b6
trdcaz <trdcaz@gmail.com>
2017-12-21 17:45:47 +0800

    Change logfile to minions about command_job

--- command_job/views.py
+++ command_job/views.py
@@ -38,9 +38,10 @@ def command_req(request):
     else:
         s_a_l_t = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(16))
         output = '%s-%s-%s-%s.log' % (zone, time.strftime('%Y%m%d.%H%M%S', time.localtime()), username, s_a_l_t)
-        output = os.path.join('/tmp', svc_name, output)
+        output = os.path.join('/var/log', svc_name, output)
         command_exec.delay(username, phone, ip, svc_name, cmd, zone, output)
-        result = [{'Command Job': 'The command is running, please check log file(%s)!' % (output,)}]
+        cmd_host = cmd_host_qcd if zone == 'qcd' else cmd_host_aws
+        result = [{'Command Job': 'The command is running, please check log file(%s:%s)!' % (cmd_host, output,)}]
 
     return render(request, 'getdata.html', {'result': result})
 
@@ -53,9 +54,10 @@ def command_exec(username, phone, ip, svc_name, cmd, zone, output):
            ' --non-interactive --username={username} --password={password} && ' \
            'svn co http://svn.65dg.me/svn/{svc_name} /srv/{svc_name} ' \
            ' --non-interactive --username={username} --password={password} && ' \
-           '/srv/{svc_name}/{cmd} -c /srv/gotemplate/{svc_name}/conf.ctmpl' \
+           'mkdir -p {log_path} && ' \
+           '/srv/{svc_name}/{cmd} -c /srv/gotemplate/{svc_name}/conf.ctmpl >> {output} 2>&1' \
            ''.format(username=svn_username, password=svn_password,
-                     svc_name=svc_name, cmd=cmd)
+                     svc_name=svc_name, cmd=cmd, output=output, log_path=os.path.dirname(output))
     print(_cmd)
 
     salt_api = SaltApi()
@@ -69,28 +71,28 @@ def command_exec(username, phone, ip, svc_name, cmd, zone, output):
     if result != 0:
         result = result['return']
 
-    if not os.path.exists(os.path.dirname(output)):
-        try:
-            os.makedirs(os.path.dirname(output))
-        except Exception as e:
-            print(e)
-            logs(username, ip, cmd, e)
-            dingding_robo(cmd_host, 'command job', ' error ', username, phone)
-            return str(e)
-
-    with open(output, 'w') as f:
-        if isinstance(result, list):
-            for r in result:
-                if isinstance(r, dict):
-                    for k, v in r.items():
-                        f.write('--> ')
-                        f.write(k)
-                        f.write('\n')
-                        f.write(v)
-                else:
-                    f.write(str(r))
-        else:
-            f.write(str(result))
+    # if not os.path.exists(os.path.dirname(output)):
+    #     try:
+    #         os.makedirs(os.path.dirname(output))
+    #     except Exception as e:
+    #         print(e)
+    #         logs(username, ip, cmd, e)
+    #         dingding_robo(cmd_host, 'command job', ' error ', username, phone)
+    #         return str(e)
+    #
+    # with open(output, 'w') as f:
+    #     if isinstance(result, list):
+    #         for r in result:
+    #             if isinstance(r, dict):
+    #                 for k, v in r.items():
+    #                     f.write('--> ')
+    #                     f.write(k)
+    #                     f.write('\n')
+    #                     f.write(v)
+    #             else:
+    #                 f.write(str(r))
+    #     else:
+    #         f.write(str(result))
 
     logs(username, ip, cmd, 'running')
     dingding_robo(cmd_host, 'command job', result, username, phone)
