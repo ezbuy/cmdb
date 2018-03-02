2893b3ae88f7b49a7c920e42db6afbcfa1565ba5
shimron <shenxq@live.com>
2018-03-02 14:32:45 +0800

    merge

--- disque/views.py
+++ disque/views.py
@@ -24,16 +24,17 @@ clientEnvMap = {
     'qcd': disqueQCD
 }
 
+default_content_type = 'application/json'
 
+
+@login_required
 def ackjob_index(request):
     return render(request, 'disque_ack_job.html')
 
 
+@login_required
 def addjob_index(request):
     return render(request, 'disque_add_job.html')
-
-
-default_content_type = 'application/json'
 
 
 @login_required
@@ -41,11 +42,11 @@ def ack_job(request):
     env = request.POST['env']
     if not (env in clientEnvMap.keys()):
         return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=default_content_type)
-    jobIds = request.POST.getlist('jobIds[]', [])
+    jobIds = request.POST.getlist('jobIds', [])
     if len(jobIds) == 0:
         return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobIds'}), content_type=default_content_type)
     jobIds = map(lambda x: x.encode('utf-8'), jobIds)
-    print env, jobIds
+    print request.user, env, jobIds
     try:
         client = clientEnvMap[env]
         client.ack_job(*jobIds)
@@ -58,28 +59,33 @@ def ack_job(request):
 @login_required
 def add_job(request):
     env = request.POST['env']
-    queue = request.POST['queue']
-    jobs = request.POST.getlist['jobs']
-    ms_timeout = request.POST['timeout']
+    queue = request.POST['queue_name']
+    timeout_ms = request.POST['timeout_ms']
     replicate = request.POST['replicate']
-    retry_sec = request.POST['retry']
-    delay_sec = request.POST['delay']
-    ttl_sec = request.POST['ttl']
-    if not (env and queue):
-        return HttpResponse('env or queue is empty')
-    if len(jobs) == 0:
-        return HttpResponse('empty jobs')
+    retry_sec = request.POST['retry_sec']
+    delay_sec = request.POST['delay_sec']
+    ttl_sec = request.POST['ttl_sec']
+    jobs = request.POST.getlist('jobs', [])
+    print request.user, env, queue, timeout_ms, replicate, retry_sec, delay_sec, ttl_sec
+    print jobs
 
     if not (env in clientEnvMap.keys()):
-        return HttpResponse('unknow disque env')
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=default_content_type)
+    if (not queue) or len(queue) == 0:
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'emtpy queue name'}), content_type=default_content_type)
+    if len(jobs) == 0:
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobs'}), content_type=default_content_type)
+
+    jobs = map(lambda x: x.encode('utf-8'), jobs)
     client = clientEnvMap[env]
     jobIds = []
     errJob = []
     for job in jobs:
         try:
-            jobId = client.add_job(queue, json.dumps(job), timeout=ms_timeout, replicate=replicate, delay=delay_sec, retry=retry_sec, ttl=ttl_sec)
+            print job
+            jobId = client.add_job(queue, job, timeout=timeout_ms, replicate=replicate, delay=delay_sec, retry=retry_sec, ttl=ttl_sec)
             jobIds.append(jobId)
         except Exception as e:
             print e
             errJob.append(job)
-    return HttpResponse(json.dumps({'jobIds': jobIds, 'failJobs': errJob}), content_type='application/json')
+    return HttpResponse(json.dumps({'errcode': 200, 'jobIds': jobIds, 'failJobs': errJob}), content_type=default_content_type)
