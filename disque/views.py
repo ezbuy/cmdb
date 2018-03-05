7449b72ab34e3795f89c4745504b36a2b83750e5
shimron <shenxq@live.com>
2018-03-05 11:18:01 +0800

    rename

--- disque/views.py
+++ disque/views.py
@@ -13,23 +13,17 @@ from asset.utils import logs, deny_resubmit
 # Create your views here.
 
 
-disqueAWS = Client(disque_aws)
-disqueAWS.connect()
+DISQUE_AWS = Client(disque_aws)
+DISQUE_QCD = Client(disque_qcd)
+DISQUE_HSG = Client(disque_hsg)
 
-disqueQCD = Client(disque_qcd)
-disqueQCD.connect()
-
-disqueHSG = Client(disque_hsg)
-disqueHSG.connect()
-
-
-clientEnvMap = {
-    'aws': disqueAWS,
-    'qcd': disqueQCD,
-    'hsg': disqueHSG
+DISQUE_CLIENT_MAP = {
+    'aws': DISQUE_AWS,
+    'qcd': DISQUE_QCD,
+    'hsg': DISQUE_HSG
 }
 
-default_content_type = 'application/json'
+DEFAULT_CONTENT_TYPE = 'application/json'
 
 
 @login_required
@@ -52,27 +46,28 @@ def ack_job(request):
     jobIds = request.POST.getlist('jobIds', [])
     if not user.groups.filter(name__in=['admin', 'dba', 'disque']).exists():
         logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, env), 'permission denied')
-        return HttpResponse(json.dumps({'errcode': 403}), content_type=default_content_type)
+        return HttpResponse(json.dumps({'errcode': 403}), content_type=DEFAULT_CONTENT_TYPE)
 
-    if not (env in clientEnvMap.keys()):
+    if not (env in DISQUE_CLIENT_MAP.keys()):
         logs(user, ip, 'ack job: %s' % jobIds, 'unknown disque zone: %s' % env)
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=default_content_type)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=DEFAULT_CONTENT_TYPE)
 
     if len(jobIds) == 0:
         logs(user, ip, 'ack job: zone-%s' % env, 'empty jobIds')
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobIds'}), content_type=default_content_type)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobIds'}), content_type=DEFAULT_CONTENT_TYPE)
     jobIds = map(lambda x: x.encode('utf-8'), jobIds)
     print user, env, jobIds
 
     try:
-        client = clientEnvMap[env]
+        client = DISQUE_CLIENT_MAP[env]
+        client.connect()
         client.ack_job(*jobIds)
     except Exception as e:
         print e
         logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, env), str(e))
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': str(e)}), content_type=default_content_type)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': str(e)}), content_type=DEFAULT_CONTENT_TYPE)
     logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, env), 'success')
-    return HttpResponse(json.dumps({'errcode': 200}), content_type=default_content_type)
+    return HttpResponse(json.dumps({'errcode': 200}), content_type=DEFAULT_CONTENT_TYPE)
 
 
 @login_required
@@ -93,21 +88,22 @@ def add_job(request):
 
     if not user.groups.filter(name__in=['admin', 'dba', 'disque']).exists():
         logs(user, ip, 'add job: %s - %s' % (env, queue), 'permission denied')
-        return HttpResponse(json.dumps({'errcode': 403}), content_type=default_content_type)
-    if not (env in clientEnvMap.keys()):
+        return HttpResponse(json.dumps({'errcode': 403}), content_type=DEFAULT_CONTENT_TYPE)
+    if not (env in DISQUE_CLIENT_MAP.keys()):
         logs(user, ip, 'add job: %s - %s' % (env, queue), 'unknown disque zone:%s' % env)
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=default_content_type)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=DEFAULT_CONTENT_TYPE)
     if (not queue) or len(queue) == 0:
         logs(user, ip, 'add job: %s - %s' % (env, queue), 'empty queue name')
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'emtpy queue name'}), content_type=default_content_type)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'emtpy queue name'}), content_type=DEFAULT_CONTENT_TYPE)
     if len(jobs) == 0:
         logs(user, ip, 'add job: %s - %s' % (env, queue), 'empty jobs')
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobs'}), content_type=default_content_type)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobs'}), content_type=DEFAULT_CONTENT_TYPE)
 
     jobs = map(lambda x: x.encode('utf-8'), jobs)
-    client = clientEnvMap[env]
     jobIds = []
     errJob = []
+    client = DISQUE_CLIENT_MAP[env]
+    client.connect()
     for job in jobs:
         try:
             print job
@@ -118,4 +114,4 @@ def add_job(request):
             print e
             errJob.append(job)
     logs(user, ip, 'add job: %s - %s' % (env, queue), 'success')
-    return HttpResponse(json.dumps({'errcode': 200, 'jobIds': jobIds, 'failJobs': errJob}), content_type=default_content_type)
+    return HttpResponse(json.dumps({'errcode': 200, 'jobIds': jobIds, 'failJobs': errJob}), content_type=DEFAULT_CONTENT_TYPE)
