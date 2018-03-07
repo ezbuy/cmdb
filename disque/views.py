c4e13cdf1c9c23c207615be6a4593cbab853ff27
shimron <shenxq@live.com>
2018-03-07 16:54:48 +0800

    read disque cluster info from db

--- disque/views.py
+++ disque/views.py
@@ -6,22 +6,12 @@ from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from pydisque.client import Client
-from mico.settings import disque_aws, disque_qcd, disque_hsg
 import json
 from django.shortcuts import render
 from asset.utils import logs, deny_resubmit
+from asset.models import DisqueCluster
 # Create your views here.
 
-
-DISQUE_AWS = Client(disque_aws)
-DISQUE_QCD = Client(disque_qcd)
-DISQUE_HSG = Client(disque_hsg)
-
-DISQUE_CLIENT_MAP = {
-    'aws': DISQUE_AWS,
-    'qcd': DISQUE_QCD,
-    'hsg': DISQUE_HSG
-}
 
 DEFAULT_CONTENT_TYPE = 'application/json'
 
@@ -29,44 +19,56 @@ DEFAULT_CONTENT_TYPE = 'application/json'
 @login_required
 @deny_resubmit(page_key='disque_ack_job')
 def ackjob_index(request):
-    return render(request, 'disque_ack_job.html')
+    disque_zone = DisqueCluster.objects.all().order_by('name')
+    return render(request, 'disque_ack_job.html', {'zone': disque_zone})
 
 
 @login_required
 @deny_resubmit(page_key='disque_add_job')
 def addjob_index(request):
-    return render(request, 'disque_add_job.html')
+    disque_zone = DisqueCluster.objects.all().order_by('name')
+    return render(request, 'disque_add_job.html', {'zone': disque_zone})
 
 
 @login_required
 def ack_job(request):
     user = request.user
     ip = request.META['REMOTE_ADDR']
-    env = request.POST['env']
+    zone = request.POST['zone']
     jobIds = request.POST.getlist('jobIds', [])
     if not user.groups.filter(name__in=['admin', 'dba', 'disque']).exists():
-        logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, env), 'permission denied')
+        logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, zone), 'permission denied')
         return HttpResponse(json.dumps({'errcode': 403}), content_type=DEFAULT_CONTENT_TYPE)
-
-    if not (env in DISQUE_CLIENT_MAP.keys()):
-        logs(user, ip, 'ack job: %s' % jobIds, 'unknown disque zone: %s' % env)
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=DEFAULT_CONTENT_TYPE)
+    try:
+        clusterInfo = DisqueCluster.objects.get(name=zone)
+        print clusterInfo.addr
+    except DisqueCluster.DoesNotExist:
+        logs(user, ip, 'ack job: %s' % jobIds, 'unknown disque zone: %s' % zone)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % zone}), content_type=DEFAULT_CONTENT_TYPE)
+    except DisqueCluster.MultipleObjectsReturned:
+        logs(user, ip, 'ack job: %s' % jobIds, 'multi objects returned for zone: %s' % zone)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'multi objects returned for zone:%s' % zone}), content_type=DEFAULT_CONTENT_TYPE)
+    except Exception as e:
+        print e
+        logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, zone), str(e))
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': str(e)}), content_type=DEFAULT_CONTENT_TYPE)
 
     if len(jobIds) == 0:
-        logs(user, ip, 'ack job: zone-%s' % env, 'empty jobIds')
+        logs(user, ip, 'ack job: zone-%s' % zone, 'empty jobIds')
         return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobIds'}), content_type=DEFAULT_CONTENT_TYPE)
     jobIds = map(lambda x: x.encode('utf-8'), jobIds)
-    print user, env, jobIds
+    print user, zone, jobIds
 
     try:
-        client = DISQUE_CLIENT_MAP[env]
+        addr = clusterInfo.addr.split(',')
+        client = Client(addr)
         client.connect()
         client.ack_job(*jobIds)
     except Exception as e:
         print e
-        logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, env), str(e))
+        logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, zone), str(e))
         return HttpResponse(json.dumps({'errcode': 400, 'msg': str(e)}), content_type=DEFAULT_CONTENT_TYPE)
-    logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, env), 'success')
+    logs(user, ip, 'ack job: %s , zone: %s' % (jobIds, zone), 'success')
     return HttpResponse(json.dumps({'errcode': 200}), content_type=DEFAULT_CONTENT_TYPE)
 
 
@@ -74,7 +76,7 @@ def ack_job(request):
 def add_job(request):
     user = request.user
     ip = request.META['REMOTE_ADDR']
-    env = request.POST['env']
+    zone = request.POST['zone']
     queue = request.POST['queue_name']
     # timeout_ms = request.POST['timeout_ms']
     # replicate = request.POST['replicate']
@@ -83,26 +85,39 @@ def add_job(request):
     # ttl_sec = request.POST['ttl_sec']
     jobs = request.POST.getlist('jobs', [])
     # print user, env, queue, timeout_ms, replicate, retry_sec, delay_sec, ttl_sec
-    print user, env, queue
+    print user, zone, queue
     print jobs
 
     if not user.groups.filter(name__in=['admin', 'dba', 'disque']).exists():
-        logs(user, ip, 'add job: %s - %s' % (env, queue), 'permission denied')
+        logs(user, ip, 'add job: %s - %s' % (zone, queue), 'permission denied')
         return HttpResponse(json.dumps({'errcode': 403}), content_type=DEFAULT_CONTENT_TYPE)
-    if not (env in DISQUE_CLIENT_MAP.keys()):
-        logs(user, ip, 'add job: %s - %s' % (env, queue), 'unknown disque zone:%s' % env)
-        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % env}), content_type=DEFAULT_CONTENT_TYPE)
+
+    try:
+        clusterInfo = DisqueCluster.objects.get(name=zone)
+        print clusterInfo.addr
+    except DisqueCluster.DoesNotExist:
+        logs(user, ip, 'add job: %s - %s' % (zone, queue), 'unknown disque zone: %s' % zone)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'unknown disque zone:%s' % zone}), content_type=DEFAULT_CONTENT_TYPE)
+    except DisqueCluster.MultipleObjectsReturned:
+        logs(user, ip, 'add job: %s - %s' % (zone, queue), 'multi objects returned for zone: %s' % zone)
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': 'multi objects returned for zone:%s' % zone}), content_type=DEFAULT_CONTENT_TYPE)
+    except Exception as e:
+        print e
+        logs(user, ip, 'add job: %s - %s' % (zone, queue), str(e))
+        return HttpResponse(json.dumps({'errcode': 400, 'msg': str(e)}), content_type=DEFAULT_CONTENT_TYPE)
+
     if (not queue) or len(queue) == 0:
-        logs(user, ip, 'add job: %s - %s' % (env, queue), 'empty queue name')
+        logs(user, ip, 'add job: %s - %s' % (zone, queue), 'empty queue name')
         return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty queue name'}), content_type=DEFAULT_CONTENT_TYPE)
     if len(jobs) == 0:
-        logs(user, ip, 'add job: %s - %s' % (env, queue), 'empty jobs')
+        logs(user, ip, 'add job: %s - %s' % (zone, queue), 'empty jobs')
         return HttpResponse(json.dumps({'errcode': 400, 'msg': 'empty jobs'}), content_type=DEFAULT_CONTENT_TYPE)
 
     jobs = map(lambda x: x.encode('utf-8'), jobs)
     jobIds = []
     errJob = []
-    client = DISQUE_CLIENT_MAP[env]
+    addr = clusterInfo.addr.split(',')
+    client = Client(addr)
     client.connect()
     for job in jobs:
         try:
@@ -113,5 +128,5 @@ def add_job(request):
         except Exception as e:
             print e
             errJob.append(job)
-    logs(user, ip, 'add job: %s - %s' % (env, queue), 'success')
+    logs(user, ip, 'add job: %s - %s' % (zone, queue), 'success')
     return HttpResponse(json.dumps({'errcode': 200, 'jobIds': jobIds, 'failJobs': errJob}), content_type=DEFAULT_CONTENT_TYPE)
