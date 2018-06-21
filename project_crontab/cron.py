348dea6d1a9c30cebbfca24df6312c3eae6f233b
HuangYao <huangyao@ezbuy.com>
2018-06-21 16:37:08 +0800

    [update]sync crontab

--- project_crontab/cron.py
+++ project_crontab/cron.py
@@ -4,10 +4,10 @@ import commands
 import sys
 import croniter
 
+from project_crontab import models
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
-from project_crontab import models
 
 
 def syncCronHost2DB():
@@ -17,6 +17,7 @@ def syncCronHost2DB():
     my_cron = CronTab(tabfile='/etc/crontab', user=False)
     for job in my_cron[4:]:
         if job.is_enabled():
+            print job.command
             try:
                 models.CrontabCmd.objects.get(auto_cmd=job.command)
             except models.CrontabCmd.DoesNotExist:
@@ -24,7 +25,11 @@ def syncCronHost2DB():
                     is_valid = 1
                 else:
                     is_valid = 2
+                print 'is_valid : '
+                print is_valid
                 frequency = str(job).split('root')[0].strip()
-                models.CrontabCmd.objects.create(project=None, cmd=job.command, auto_cmd=job.command, is_valid=is_valid, frequency=frequency, cmd_status=2, last_run_time=None, last_run_result=None)
+                print 'frequency : '
+                print frequency
+                # models.CrontabCmd.objects.create(project=None, cmd=job.command, auto_cmd=job.command, is_valid=is_valid, frequency=frequency, cmd_status=2, last_run_time=None, last_run_result=None)
             else:
                 print 'already exist in DB'
