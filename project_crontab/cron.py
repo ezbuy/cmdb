1fc684e2566ca1ffcbd8b836856fb5c763183197
HuangYao <huangyao@ezbuy.com>
2018-06-21 17:28:29 +0800

    [update]sync crontab

--- project_crontab/cron.py
+++ project_crontab/cron.py
@@ -18,6 +18,7 @@ def syncCronHost2DB():
     for job in my_cron[4:]:
         if job.is_enabled():
             print job.command
+            print type(job.command)
             try:
                 models.CrontabCmd.objects.get(auto_cmd=job.command)
             except models.CrontabCmd.DoesNotExist:
@@ -25,11 +26,10 @@ def syncCronHost2DB():
                     is_valid = 1
                 else:
                     is_valid = 2
-                print 'is_valid : '
-                print is_valid
                 frequency = str(job).split('root')[0].strip()
-                print 'frequency : '
                 print frequency
+                print type(frequency)
+                print ' '
                 # models.CrontabCmd.objects.create(project=None, cmd=job.command, auto_cmd=job.command, is_valid=is_valid, frequency=frequency, cmd_status=2, last_run_time=None, last_run_result=None)
             else:
                 print 'already exist in DB'
