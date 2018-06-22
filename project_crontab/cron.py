e9f6808f295216488134bed1fd76216b79abe480
HuangYao <huangyao@ezbuy.com>
2018-06-22 12:18:36 +0800

    [update]cmd field TextField

--- project_crontab/cron.py
+++ project_crontab/cron.py
@@ -26,35 +26,39 @@ def syncCronHost2DB():
     else:
         my_cron = CronTab(tabfile='/etc/crontab', user=False)
         for job in my_cron[4:]:
-            if job.is_enabled():
+            try:
+                models.CrontabCmd.objects.get(auto_cmd=job.command)
+            except models.CrontabCmd.DoesNotExist:
+                if job.is_valid():
+                    is_valid = 1
+                else:
+                    is_valid = 2
+
+                if job.is_enabled():
+                    cmd_status = 2
+                else:
+                    cmd_status = 1
+
+                last_run_time = job.schedule().get_prev()
+
+                if '*' in str(job):
+                    frequency = ' '.join(str(job).strip('#').strip().split(' ')[0:5])
+                else:
+                    frequency = str(job).strip('#').strip().split(' ')[0]
+
                 try:
-                    models.CrontabCmd.objects.get(auto_cmd=job.command)
-                except models.CrontabCmd.DoesNotExist:
-                    if job.is_valid():
-                        is_valid = 1
-                    else:
-                        is_valid = 2
-
-                    last_run_time = job.schedule().get_prev()
-
-                    if '*' in str(job):
-                        frequency = ' '.join(str(job).strip('#').strip().split(' ')[0:5])
-                    else:
-                        frequency = str(job).strip('#').strip().split(' ')[0]
-
+                    svn_obj = models.Svn.objects.get(salt_minion=salt_obj)
+                except models.Svn.DoesNotExist:
+                    print ' '
+                    print job.command
+                    print 'Svn.DoesNotExist'
+                else:
                     try:
-                        svn_obj = models.Svn.objects.get(salt_minion=salt_obj)
-                    except models.Svn.DoesNotExist:
+                        models.CrontabCmd.objects.create(svn=svn_obj, cmd=job.command, auto_cmd=job.command, frequency=frequency, cmd_status=cmd_status, is_valid=is_valid, last_run_time=last_run_time)
+                    except Exception as e:
                         print ' '
                         print job.command
-                        print 'Svn.DoesNotExist'
-                    else:
-                        try:
-                            models.CrontabCmd.objects.create(svn=svn_obj, cmd=job.command, auto_cmd=job.command, frequency=frequency, cmd_status=2, is_valid=is_valid, last_run_time=last_run_time)
-                        except Exception as e:
-                            print ' '
-                            print job.command
-                            print 'create Exception---', traceback.print_exc()
-                else:
-                    print 'ct cmd already exist in DB'
+                        print 'create Exception---', traceback.print_exc()
+            else:
+                print 'ct cmd already exist in DB'
     return res
